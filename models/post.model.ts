d47b0a8cf5d6779f6f4653abf8e11bbb870fb7b5
MSUResult <shivanshsingh4539@gmail.com>
2024-07-14 16:31:12 +0530

    Your descriptive commit message

--- models/post.model.ts
+++ models/post.model.ts
@@ -1,13 +1,12 @@
 import mongoose, { Document, Model } from "mongoose";
 import { IUser } from "./user.model";
 import { IComment } from "./comment.model";
-// import { IComment } from "./comment.model";
 export interface IPost{
     description:string,
     user:IUser,
     imageUrl?:string,
     likes?:string[],
-    comments:IComment
+    comments?:IComment[]
 }
 export interface IPostDocument extends IPost, Document{
     createdAt:Date,
